b828e6742e94805bdbb9981729633f3ae52a488d
Kerem Karadag <akaradag32@gmail.com>
2023-09-26 19:53:57 +0200

    finished

--- KeremFirstLab/src/App.jsx
+++ KeremFirstLab/src/App.jsx
@@ -10,8 +10,47 @@ import menu from './assets/menu-top-xs.png';
 function App() {
   return (
     <div className='App'>
-      <img src={logo}></img>
-      <img src={menu}></img>
+      <div className='upperDiv'>
+        <header>
+          <div className='topbar'>
+            <img src={logo} style={{ padding: 10 + 'px ' + 50 + 'px' }}></img>
+            <img
+              src={menu}
+              style={{ alignSelf: 'flex-end', padding: 10 + 'px ' + 50 + 'px' }}
+            ></img>
+          </div>
+        </header>
+        <div style={{ padding: 50 + 'px' }}>
+          <h1>Say Hello to ReactJs</h1>
+          <p>
+            You will learn how to use <br></br> the most populer frontend
+            library, <br></br> and become a super ninja developer
+          </p>
+          <button>awesome</button>
+        </div>
+      </div>
+      <div className='lowerDiv'>
+        <div className='oneBlock'>
+          <img src={icon1} alt='' />
+          <h2>Declerative</h2>
+          <a>react makes it painless to create interactive UIs</a>
+        </div>
+        <div className='oneBlock'>
+          <img src={icon2} alt='' />
+          <h2>Declerative</h2>
+          <a>react makes it painless to create interactive UIs</a>
+        </div>
+        <div className='oneBlock'>
+          <img src={icon3} alt='' />
+          <h2>Declerative</h2>
+          <a>react makes it painless to create interactive UIs</a>
+        </div>
+        <div className='oneBlock'>
+          <img src={icon4} alt='' />
+          <h2>Declerative</h2>
+          <a>react makes it painless to create interactive UIs</a>
+        </div>
+      </div>
     </div>
   );
 }
